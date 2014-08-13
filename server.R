88e3ff6344e82500487c350a23fa85634c00296d
dnaeye <dnaeye@gmail.com>
2014-08-13 15:09:44 -0700

    Update server.R

--- server.R
+++ server.R
@@ -1,22 +1,32 @@
 library(shiny)
-library(rpart)
-library(caret)
-data(iris)
+library(datasets)
+# load USArrests dataset from the datasets library
+crimes <-data.frame(state = tolower(rownames(USArrests)), USArrests)
+# create a dataframe of state coordinates
+states_map <-map_data("state")
 
-irisPredict <- function(length, width) {
-    inTrain <- createDataPartition(iris$Species, p=0.75, list = FALSE)
-    training <- iris[inTrain,]
-    testing <- iris[-inTrain,]
+library(ggplot2)
+# load helpers script that contains main mapping function, ratemap
+source("helpers.R")
+
+shinyServer(function(input, output){
     
-    modelFit <- train(training$Species~., method='rpart', data=training)
-    testSet <- data.frame(Sepal.Length=0, Sepal.Width=0, Petal.Length=length, Petal.Width=width)
-    predict(modelFit, testSet)
-}
-
-shinyServer(
-    function(input, output){
-        output$inputValue1 <- renderPrint({input$length})
-        output$inputValue2 <- renderPrint({input$width})
-        output$prediction <- renderPrint({irisPredict(input$length, input$width)})
-    }
-)
+    # create reactive map that changes based on live user input
+    output$map <- renderPlot({
+        data <- switch(input$var,
+                        "Assault" = crimes[,c("state", "Assault")],
+                        "Murder" = crimes[,c("state", "Murder")],
+                        "Rape" = crimes[,c("state", "Rape")])
+        values <- switch(input$var,
+                         "Assault" = crimes$Assault,
+                         "Murder" = crimes$Murder,
+                         "Rape" = crimes$Rape)
+        color <- switch(input$var,
+                        "Assault" = "darkgreen",
+                        "Murder" = "red",
+                        "Rape" = "darkviolet")
+        
+        # call to main mapping function, ratemap
+        ratemap(data, values, color)
+    })
+})
