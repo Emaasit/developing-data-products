b879b0dc94d306012b9e6228509ac34417fe24a0
dnaeye <dnaeye@gmail.com>
2014-08-14 07:51:32 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,5 +1,8 @@
 # ui.R
-# simple user interface for getting user input on his/her desired
+# Author: dnaeye (https://github.com/dnaeye)
+# Version: 1.0
+# Created: 8/14/2014
+# This is a simple user interface for getting user input on his/her desired
 # violent crime to display as a choropleth map of the United States.
 
 shinyUI(fluidPage(
@@ -8,7 +11,8 @@ shinyUI(fluidPage(
     sidebarLayout(
         sidebarPanel(
             helpText("Compare violent crime rates for assault, murder, 
-                     and rape in each of the 50 U.S. states in 1973."),
+                     and rape in each of the 50 U.S. states in 1973.
+                     Source: USArrests dataset in datasets library"),
             
             # selectInput widget for getting user input
             selectInput("var", 
